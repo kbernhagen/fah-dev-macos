0959d8527e60a148309c2db36cd24c2fb4aa4e8b
Kevin Bernhagen <6762784+kbernhagen@users.noreply.github.com>
2021-08-24 16:07:45 -0700

    Clean more

--- bin/clean-fah.sh
+++ bin/clean-fah.sh
@@ -6,16 +6,18 @@ cd "$(dirname "$0")"
 
 cd "$CBANG_HOME" && scons --clean
 cd "$LIBFAH_HOME" && scons --clean
-cd "$FAH_VIEWER_HOME" && scons --clean
-cd "$FAH_CLIENT_HOME" && scons --clean
-cd "$FAH_CLIENT_BASTET_HOME" && scons --clean
-cd "$FAH_CONTROL_HOME" && scons --clean
+cd "$FAH_VIEWER_HOME" && scons --clean && scons --clean package
+cd "$FAH_CLIENT_HOME" && scons --clean && scons --clean package
+cd "$FAH_CLIENT_BASTET_HOME" && scons --clean && scons --clean package
+cd "$FAH_CONTROL_HOME" && scons --clean && scons --clean package
 
-cd "$FAH_WEB_CLIENT_HOME" && scons --clean || true # fails
+cd "$FAH_WEB_CLIENT_HOME" && scons --clean || true # partly fails
 rm -r "$FAH_WEB_CLIENT_HOME"/resources.{cpp,data,o}
 rm -r "$FAH_WEB_CLIENT_HOME"/libfah-web-client-resources.a
 
 cd "$FAH_CLIENT_OSX_UNINSTALLER_HOME" && scons --clean distclean
+
+# don't delete final dist packages and zip files
 cd "$FAH_CLIENT_OSX_INSTALLER_HOME" && scons --clean
 cd "$FAH_CLIENT_BASTET_OSX_INSTALLER_HOME" && scons --clean
 
